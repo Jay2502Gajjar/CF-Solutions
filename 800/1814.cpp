0feabb69d3fe7ab9a2c26c8fd8d3ea70b856eff9
Jay2502Gajjar <70512023+Jay2502Gajjar@users.noreply.github.com>
2026-01-16 22:18:25 +0530

    Add files via upload

---
+++ 800/1814.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+bool solve(ll n,ll k){
+        if(n%2==0 or n%k==0){
+            return true;
+        }
+        else if(n%2 and k%2){
+            return true;
+        }
+        else{
+            return false;
+        }
+}
+int main() {
+    int t;
+    cin>>t;
+
+
+    while(t--){
+        ll n,k;
+        cin>>n>>k;
+        cout<<(solve(n,k)?"YES":"NO");
+        cout<<endl;
+    }    
+}
